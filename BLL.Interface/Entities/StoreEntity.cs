52e10c0d0733dc3245cf34520b3b493a4bd13e5b
Castorg <rumata.it@gmail.com>
2015-01-16 15:46:20 +0300

    11

---
+++ BLL.Interface/Entities/StoreEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Interface.Entities
+{
+    public class StoreEntity
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Addres { get; set; }
+    }
+}
---
+++ BLL.Interface/Services/IService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL.Interface.Repository;
+
+namespace BLL.Interface.Services
+{
+    public interface IService
+    {
+        IUnitOfWork UnitOfWork { get; }
+    }
+
+    public interface IService<T> : IService where T : class
+    {
+        List<T> GetAll();
+        void Insert(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
