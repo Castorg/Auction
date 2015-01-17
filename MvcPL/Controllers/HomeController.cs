f18f64143e5d5269c0a4e488e23231beb4c5c6ef
Castorg <rumata.it@gmail.com>
2015-01-17 22:54:26 +0300

    попытка 42

---
+++ MvcPL/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BLL.Interface.Services;
+
+namespace MvcPL.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private readonly IUserService _userService;
+        private readonly ILotService _lotService;
+        private readonly IStoreService _storeService;
+        private readonly IRoleService _roleService;
+
+
+        public HomeController()
+        {
+            int v = 0;
+        }
+        public HomeController(IUserService userService, ILotService lotService ,IStoreService storeService ,IRoleService roleService)
+        {
+            this._userService = userService;
+            this._lotService = lotService;
+            this._roleService = roleService;
+            this._storeService = storeService;
+            int a = 0;
+        }
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ MvcPL/Infrastructura/NinjectDependencyResolver.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DependencyResolver;
+using Ninject;
+
+namespace MvcPL.Infrastructura
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private readonly IKernel _kernel;
+        public NinjectDependencyResolver()
+        {
+            _kernel = new StandardKernel(new ResolverModule());
+        }
+        public object GetService(Type serviceType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
