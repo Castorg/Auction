1e33d0ab30c5301faac525c8ff005b661b4ef8c8
Castorg <rumata.it@gmail.com>
2015-01-18 18:03:26 +0300

    ува

--- MvcPL/Controllers/HomeController.cs
+++ MvcPL/Controllers/HomeController.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using BLL.Interface.Services;
+using CustomORM;
+
 
 namespace MvcPL.Controllers
 {
@@ -26,14 +24,32 @@ namespace MvcPL.Controllers
             this._lotService = lotService;
             this._roleService = roleService;
             this._storeService = storeService;
-            int a = 0;
         }
         //
         // GET: /Home/
 
         public ActionResult Index()
         {
-            return View();
+            #region Store
+            /*
+            var st = new Store {Name = "Kupi-Prodau", Addres = "tam", StoreId = 2};
+            //_storeService.Repository.Create(new Store{Name = "PROstore" ,Addres = "tyt" ,StoreId = 1});
+
+            _lotService.Repository.Create(new Lot { CurrentCost = 121 , Description = "Патефон \"дружба\"" , LotId = 1 , Store = st });
+            _lotService.Repository.Create(new Lot { CurrentCost = 144, Description = "Браслет \"Дары смерти\"", LotId = 2, Store = st });
+            _lotService.Repository.Create(new Lot { CurrentCost = 169, Description = "Гримерное зеркало", LotId = 3, Store = st });
+            _lotService.Repository.Create(new Lot { CurrentCost = 196, Description = "Штык нож \"Жаба\"", LotId = 4, Store = st });
+            _lotService.Repository.Create(new Lot { CurrentCost = 225, Description = "3 копейки 1915 г", LotId = 5, Store = st });
+            _lotService.Repository.Create(new Lot { CurrentCost = 256, Description = "2 копейки 1905 г", LotId = 6, Store = st });
+            _lotService.Repository.Create(new Lot { CurrentCost = 289, Description = "10 копеек 1914 г СПБ ВС", LotId = 7, Store = st });
+            _lotService.Repository.Create(new Lot { CurrentCost = 324, Description = "Автокресло Maxi-cosi Pebble", LotId = 8, Store = st });
+            _lotService.UnitOfWork.Commit();*/
+
+            var sdf = _lotService.Repository.GetByPredicate();
+            #endregion
+            int a = 0;
+
+            return View(sdf);
         }
 
     }
